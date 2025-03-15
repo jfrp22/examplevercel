11e4e40ce8328f802c2e42f94ca86533cf87e994
Jaime Ramirez <esp8266tg@gmail.com>
2025-03-15 11:36:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde cualquier origen
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Almacenar datos en memoria
 let latestData = null;
